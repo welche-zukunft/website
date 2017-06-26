7275b9614f6ec6b095178e965c9f82212a699c2e
alexh <git@alexh.name>
2017-06-26 12:42:58 +0200

    evolve.js: basic shape based text

--- js/evolveline.js
+++ js/evolveline.js
@@ -40,6 +40,35 @@ function init() {
 	stats = new Stats();
 	container.appendChild( stats.dom );			
 
+	var fontloader = new THREE.FontLoader();
+	fontloader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+		var xMid, text;
+		var textShape = new THREE.BufferGeometry();
+		var color = 0x006699;
+		var matDark = new THREE.LineBasicMaterial( {
+			color: color,
+			side: THREE.DoubleSide
+		} );
+//		var matLite = new THREE.MeshBasicMaterial( {
+//			color: color,
+//			transparent: true,
+//			opacity: 0.4,
+//			side: THREE.DoubleSide
+//		} );
+		var message = "   Three.js\nSimple text.";
+		var shapes = font.generateShapes( message, 100, 2 );
+		var geometry = new THREE.ShapeGeometry( shapes );
+		geometry.computeBoundingBox();
+		xMid = -0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
+		geometry.translate( xMid, 0, 0 );
+		// make shape ( N.B. edge view not visible )
+		textShape.fromGeometry( geometry );
+//		text = new THREE.Mesh( textShape, matLite );
+		text = new THREE.Mesh( textShape, matDark );
+		text.position.z = 0;
+		scene.add( text );
+
+	} ); //end load function
 }
 
 // PRESS "1" TO (UN)REVEAL POINTERs ---------------------------------

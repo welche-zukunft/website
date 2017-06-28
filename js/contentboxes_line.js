ed764c488b5b029fc6484102507755e60404e055
alexh <git@alexh.name>
2017-06-28 22:00:54 +0200

    contentboxes_line: put functions in own file

--- js/contentboxes_line.js
+++ js/contentboxes_line.js
@@ -15,64 +15,17 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
+  add_sphere("white");
 
-  sphereObj = function( color ) {
-    var sphGeom = new THREE.SphereGeometry(0.5, 8, 4);
-    var sphMat = new THREE.MeshBasicMaterial({
-      color: color,
-      wireframe: true
-    });
-    return new THREE.Mesh(sphGeom, sphMat);
-  }
+  // content_line_draw(sObj_pos,dObj_pos,color)
+  content_line_draw(content_sObj.position,content_dObj_pos,"fuchsia");
 
-  content_sObj = new sphereObj("white");
-
-  var content_lineGeom = new THREE.Geometry();
-
-  content_lineGeom.vertices.push(content_sObj.position);
-  content_lineGeom.vertices.push(content_dObj_pos);
-  var content_lineMat = new THREE.LineBasicMaterial({
-    color: "fuchsia"
-  });
-  content_line = new THREE.Line(content_lineGeom, content_lineMat);
-
-  scene.add(content_sObj);
-  scene.add(content_line);
-}
-
-function contentbox_line() {
-  // source position
-  var timestamp = new Date() * 0.0005;
-  content_sObj.position.x = Math.cos(timestamp) * 7;
-  content_sObj.position.z = Math.sin(timestamp) * 7;
-
-  // vector for handle pos
-  var vector = new THREE.Vector3();
-
-  vector.set(
-    ( handle_pos.x / window.innerWidth ) * 2 - 1,
-    - ( handle_pos.y / window.innerHeight ) * 2 + 1,
-    0.5
-  );
-
-  vector.unproject( camera );
-  var dir = vector.sub( camera.position ).normalize();
-  var content_targetZ = 0;
-  var distance = (content_targetZ - camera.position.z) / dir.z;
-  pos = camera.position.clone().add( dir.multiplyScalar( distance ) );
-  //
-
-  content_dObj_pos.x = pos.x;
-  content_dObj_pos.y = pos.y;
-  content_dObj_pos.z = pos.z;
-  //console.log( content_dObj_pos );
-  content_line.geometry.verticesNeedUpdate = true;
 }
 
 function animate() {
   requestAnimationFrame(animate);
 
-  contentbox_line();
+  content_line_pos();
 
   renderer.render(scene, camera);
 }
---
+++ js/contentboxes_line_functions.js
@@ -0,0 +1,56 @@
+function content_line_draw(sObj_pos,dObj_pos,color) {
+  var lineGeom = new THREE.Geometry();
+
+  lineGeom.vertices.push(sObj_pos);
+  lineGeom.vertices.push(dObj_pos);
+  var lineMat = new THREE.LineBasicMaterial({
+    color: color
+  });
+  content_line = new THREE.Line(lineGeom, lineMat);
+
+  scene.add(content_line);
+}
+
+function content_line_pos() {
+  // source position
+  var timestamp = new Date() * 0.0005;
+  content_sObj.position.x = Math.cos(timestamp) * 7;
+  content_sObj.position.z = Math.sin(timestamp) * 7;
+
+  // vector for handle pos
+  var vector = new THREE.Vector3();
+
+  vector.set(
+    ( handle_pos.x / window.innerWidth ) * 2 - 1,
+    - ( handle_pos.y / window.innerHeight ) * 2 + 1,
+    0.5
+  );
+
+  vector.unproject( camera );
+  var dir = vector.sub( camera.position ).normalize();
+  var content_targetZ = 0;
+  var distance = (content_targetZ - camera.position.z) / dir.z;
+  pos = camera.position.clone().add( dir.multiplyScalar( distance ) );
+  //
+
+  content_dObj_pos.x = pos.x;
+  content_dObj_pos.y = pos.y;
+  content_dObj_pos.z = pos.z;
+  //console.log( content_dObj_pos );
+  content_line.geometry.verticesNeedUpdate = true;
+}
+
+function add_sphere(color) {
+  sphereObj = function( color ) {
+    var sphGeom = new THREE.SphereGeometry(0.5, 8, 4);
+    var sphMat = new THREE.MeshBasicMaterial({
+      color: color,
+      wireframe: true
+    });
+    return new THREE.Mesh(sphGeom, sphMat);
+  }
+
+  content_sObj = new sphereObj(color);
+  scene.add(content_sObj);
+}
+

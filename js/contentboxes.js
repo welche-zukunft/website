4a93766d49141ced61f8efc6d409ba3795c727d4
alexh <git@alexh.name>
2017-06-29 20:19:10 +0200

    js/contentboxes.js: get contents from json

--- js/contentboxes.js
+++ js/contentboxes.js
@@ -1,16 +1,15 @@
-var contents = [
-  "Oh HAI",
-  "halloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooomelone",
-  "Kirschtorte",
-  "Schokotee!",
-  '<iframe width="560" height="315" src="https://www.youtube.com/embed/7VG_s2PCH_c" frameborder="0" allowfullscreen></iframe>',
-  "<h2>Überschrift</h2><br>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.   Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.   Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.   At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur"
-];
+//var contents_json = '{ "date":"123", "contents":[ "arma", "virumque", "cano" ] }';
 
-contents = [
-  "1",
-  "2"
-]
+var url = "http://0101010.one/cgi-bin/cgi-json.sh";
+
+$.get( url, jsontocontents );
+
+var contents;
+
+function jsontocontents(data) {
+  console.log(data);
+  contents = JSON.parse(JSON.stringify(data)); 
+}
 
 function contentbox_create(num) {
   // create dragbox, contenbox, handle
@@ -51,7 +50,7 @@ function contentbox_create(num) {
     //$( contentbox ).resizable();
   });
   // fill contentbox with content
-  contentbox.innerHTML = '<div class="nodrag content">' + contents[num] + '</div>';
+  contentbox.innerHTML = '<div class="nodrag content">' + contents.contents[num] + '</div>';
   // add dragbox to div "contentplane"
   document.body.appendChild(dragbox);
   // add contentbox to dragbox
@@ -62,7 +61,7 @@ function contentbox_create(num) {
 
 // iterate over available content to create boxes for them
 function contentbox_create_all() {
-  for (i = 0; i < contents.length; i++) { 
+  for (i = 0; i < contents.contents.length; i++) { 
     contentbox_create(i);
   }
 }

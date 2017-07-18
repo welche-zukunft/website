24d874606aeea3176f7f02aa3bdb852141c128ad
alexh <git@alexh.name>
2017-07-18 12:29:40 +0200

    use raycaster to convert 2d pos to world

--- js/contentboxes_line_functions.js
+++ js/contentboxes_line_functions.js
@@ -18,31 +18,28 @@ function content_line_draw(sObj_pos,dObj_pos,color) {
 }
 
 function content_line_pos() {
-  // source position
-  //var timestamp = new Date() * 0.0005;
-  //content_sObj.position.x = Math.cos(timestamp) * 7;
-  //content_sObj.position.z = Math.sin(timestamp) * 7;
-
   // vector for handle pos
-  var vector = new THREE.Vector3();
+  var vector = new THREE.Vector2();
+  var raycaster = new THREE.Raycaster(); // create once
+  // plane representing the windows
+  var planeZ = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
 
-  vector.set(
-    ( handle_pos.x / window.innerWidth ) * 2 - 1,
-    - ( handle_pos.y / window.innerHeight ) * 2 + 1,
-    0.5
-  );
+  vector.x = ( handle_pos.x / renderer.domElement.clientWidth ) * 2 - 1;
+  vector.y = - ( handle_pos.y / renderer.domElement.clientHeight ) * 2 + 1;
 
-  vector.unproject( camera );
-  var dir = vector.sub( camera.position ).normalize();
-  var content_targetZ = 0;
-  var distance = (content_targetZ - camera.position.z) / dir.z;
-  pos = camera.position.clone().add( dir.multiplyScalar( distance ) );
-  //
+  raycaster.setFromCamera( vector, camera );
 
-  content_dObj_pos.x = pos.x;
-  content_dObj_pos.y = pos.y;
-  content_dObj_pos.z = pos.z;
-  //console.log( content_dObj_pos );
+  var intersect = raycaster.ray.intersectPlane( planeZ );
+
+  pos = intersect;
+
+  // sometimes, there is no intersection with the plane (?)
+  // so we update position only when there is one
+  if ( pos != null ) {
+    content_dObj_pos.x = pos.x;
+    content_dObj_pos.y = pos.y;
+  };
+
   content_line.geometry.verticesNeedUpdate = true;
 }
 

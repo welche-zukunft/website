031e833a33ce73ef63d7172cc16f9712d528b5ce
alexh <git@alexh.name>
2017-07-31 18:14:30 +0200

    WS form: map responses to nice messages

--- js/form_submit.js
+++ js/form_submit.js
@@ -1,5 +1,36 @@
 var result = 'Anmeldung fehlgeschlagen. Bitte versuchen sie es später erneut.';
 
+function msg_map(msg) {
+  var result;
+  switch(msg) {
+    case "success":
+        result = 'Anmeldung erfolgreich. Bitte bestätigen Sie die Email, die sie in Kürze erhalten.';
+        break;
+    case "agb not accepted":
+        result = 'Sie müssen die AGB akzeptieren.';
+        break;
+    case "agb not accepted":
+        result = 'Sie müssen die AGB akzeptieren.';
+        break;
+    case "mail addresses do not match":
+        result = 'Email-Adressen stimmen nicht überein.';
+        break;
+    case "not a valid mail address":
+        result = 'Bitte geben Sie ein gültige Email-Adresse an.';
+        break;
+    default:
+        result = 'Es ist ein Fehler aufgetreten.';
+        break;
+  }
+  var patt = new RegExp("No message available");
+  switch(true) {
+    case patt.test(msg):
+        result = 'Bitte wählen Sie einen Workshop.';
+        break;
+  }
+  return result;
+}
+
 $(document).ready(function(){
   // WS form
   var $form = $('#anmeldung_form');
@@ -20,10 +51,12 @@ $(document).ready(function(){
       contentType: "application/json; charset=utf-8",
       success: function(response){
         result = response.responseText;
+        result = msg_map(result);
         result_label.html(result);
       },
       error: function(response){
         result = response.responseText;
+        result = msg_map(result);
         result_label.html(result);
       }
     });
